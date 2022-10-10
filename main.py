671f56b64dea04bbbcd2b307bd13e1843c3c4b89
Amgadi <sahar@getbalance.com>
2022-10-10 16:29:42 +0300

    reqs

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from score_wallet import get_wallet_data
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/wallet/stats")
 async def root(wallet_id: str):
